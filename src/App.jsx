dde4172ec7d316a2337ed603204d360f2fcd6215
Sah-Abhishek <abhisheksah3333@gmail.com>
2024-05-27 07:20:25 +0530

    added logo and applied routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Login from './components/Login'
@@ -12,24 +12,26 @@ import Header from './components/Header'
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <><Navbar /><Home /></>
-    },{
-      path: "/login",
-      element: <><Navbar /><Login /></>
-    },
-    {
-      path: "/about",
-      element: <><Navbar /><About /></>
-    }
-  ])
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <><Navbar /><Home /></>
+  //   },{
+  //     path: "/login",
+  //     element: <><Navbar /><Login /></>
+  //   },
+  //   {
+  //     path: "/about",
+  //     element: <><Navbar /><About /></>
+  //   }
+  // ])
   
   return (
     <>
     {/* <Header /> */}
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <Navbar />
+      Hi there this is the landing page
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ContactUs from './components/ContactUs.jsx'
+import Login from './components/Login.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/contactus",
+    element: <ContactUs />
+  },
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <><Navbar /> <Login /></>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>,
 )
